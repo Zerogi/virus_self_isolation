91e4e557557cfe95a229ff9c618f985ead9bd8e2
Zerogi <rob.zgonnikov@gmail.com>
2020-05-17 00:31:50 +0300

    Changes

--- site/222.py
+++ site/222.py
@@ -2,22 +2,22 @@ from data import db_session
 from data.models import User, Detection
 
 
-db_session.global_init("try.sqlite")
+db_session.global_init("test.sqlite")
 
 session = db_session.create_session()
 
 
 user_1 = User()
-user_1.name = "Пользовате§ль 1"
-user_1.surname = "11§"
-user_1.email = "user_§1@email.ru"
-user_1.set_password('s§hit')
+user_1.name = "Пользоватеasdf§ль 1"
+user_1.surname = "asdfasdf§"
+user_1.email = "user_§1@emasdfasfdail.ru"
+user_1.set_password('s§hadfasdfit')
 
-damn = Detection()
-damn.number_of_people = 10
-damn.distance_violation = 2
+#damn = Detection()
+#damn.number_of_people = 10
+#damn.distance_violation = 2
 
-user_1.detections.append(damn)
+#user_1.detections.append(damn)
 
 session.add(user_1)
 
@@ -25,7 +25,10 @@ session.commit()
 
 print('!')
 for i in session.query(User):
-    print(i)
-    #session.delete(i)
+    #print(i)
+    session.delete(i)
 
-#session.commit()
+session.commit()
+
+for i in session.query(User):
+    print(i)
--- site/app.py
+++ site/app.py
@@ -4,10 +4,12 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, InputRequired, Length
 from flask_login import login_required, logout_user, current_user, LoginManager, login_user
 from flask_mail import Mail, Message
+import json
 
 from data import db_session
 from data.models import User, Detection
 
+from random import choice
 import datetime
 
 
@@ -22,6 +24,7 @@ app.config['MAIL_USERNAME'] = 'virus.self.isolation@gmail.com'
 app.config['MAIL_DEFAULT_SENDER'] = 'virus.self.isolation@gmail.com'
 app.config['MAIL_PASSWORD'] = 'V!13wirM_TR'
 
+mail = Mail(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -41,6 +44,24 @@ class LoginForm(FlaskForm):
     submit_sign = SubmitField('Войдите')
 
 
+class CheckEmailForm(FlaskForm):
+    code = StringField('Код подтверждения',
+                             validators=[Length(min=6, max=6, message='код состоит из 6 символов')])
+    submit_code = SubmitField('Подтвердить')
+
+
+users_codes = {}
+
+
+def send_code(user):
+    code = get_code()
+    users_codes[user.id] = code
+
+    msg = Message("Подтверждение почты", recipients=[user.email])
+    msg.body = f"Ваш код подтверждения: {code}"
+    mail.send(msg)
+
+
 @login_manager.user_loader
 def load_user(user_id):
     session = db_session.create_session()
@@ -58,13 +79,11 @@ def logout():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print(form.submit_res.data)
-        print(form.submit_sign.data)
         if form.submit_res.data:
             session = db_session.create_session()
             if session.query(User).filter(User.email == form.mail.data).first():
                 return render_template("login.html", form=form,
-                                       message="Пользователь с таким email уже есть")
+                                       message="Пользователь с такой почтой уже есть")
             user = User()
             user.name = form.name.data
             user.surname = form.surname.data
@@ -74,9 +93,17 @@ def login():
             session.add(user)
             session.commit()
 
+            try:
+                send_code(user=user)
+            except:
+                session.delete(user)
+                session.commit()
+                return render_template("login.html", form=form,
+                                       message="Почта введена некорректно")
+
             #login_user(user)
 
-            return redirect(url_for('.check', user=user))
+            return redirect(url_for('.check', user=user.id))
 
         elif form.submit_sign.data:
             pass
@@ -84,10 +111,21 @@ def login():
 
 
 @app.route('/check', methods=['GET', 'POST'])
-def check(user):
-    session = db_session.create_session()
-    print(current_user.get_id())
-    return str(current_user)
+def check():
+    user_id = request.args['user']
+    user = load_user(user_id)
+
+    form = CheckEmailForm()
+    print(user.name)
+    if form.validate_on_submit():
+        if form.submit_code.data:
+            if form.submit_code.data == users_codes[user.id]:
+                print(123)
+            return user.name
+    print(user.id)
+    return render_template('/check.html', form=form)
+        # print(current_user.get_id())
+    #return render_template('/login')
 
 
 @app.route('/sign_in', methods=['GET', 'POST'])
@@ -95,15 +133,18 @@ def sign_in():
     pass
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    session = db_session.create_session()
-    return session.query(User).get(user_id)
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+
+
+def get_code():
+    nums = [str(n) for n in range(10)]
+    return ''.join([choice(nums) for _ in range(6)])
 
 
 def main():
     db_session.global_init("test.sqlite")
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='127.0.0.1', port=8000)
 
 
 if __name__ == '__main__':
